c582ad37e68927488cbfdb0131ad88a53fac97a1
Paszymaja <36695377+Paszymaja@users.noreply.github.com>
2021-04-25 13:17:12 +0200

    dunno why it works

--- main.py
+++ main.py
@@ -39,59 +39,84 @@ class Image:
     def image_mask(self):
         return np.zeros_like(self.image_gray)
 
+    def image_copy(self):
+        return np.zeros(self.image.shape, np.uint8)
+
 
 face_1 = Image('TestPhotos/Korwin.png')
 face_2 = Image('TestPhotos/Maklowicz.jpg')
-img2_new_face = np.zeros_like(face_2.image)
-height, width, channels = face_2.image.shape
-img2_new_face = np.zeros((height, width, channels), np.uint8)
+face_2_copy = face_2.image_copy()
+
+
+class FaceSwap(Image):
+    def __init__(self, image_path, detector=dlib.get_frontal_face_detector(),
+                 predictor=dlib.shape_predictor('models/shape_predictor_68_face_landmarks.dat')):
+        super().__init__(image_path)
+        self.detector = detector
+        self.predictor = predictor
+        self.faces = self.load_smth()
+
+    def load_face(self):
+        return self.detector(self.image_gray)
+
+    def load_smth(self):
+        faces = [face for face in self.load_face()]
+        return faces
+
+    def landmarks_points(self):
+        global landmarks_points
+        for face in self.faces:
+            landmarks = self.predictor(self.image_gray, face)
+            landmarks_points = []
+            for n in range(0, 68):
+                x = landmarks.part(n).x
+                y = landmarks.part(n).y
+                landmarks_points.append((x, y))
+        return landmarks_points
+
+    def smth_faces(self):
+        for _ in self.load_face():
+            landmarks_points = self.landmarks_points()
+
+            points = np.array(landmarks_points, np.int32)
+            convexhull = cv2.convexHull(points)
+            # cv2.polylines(img, [convexhull], True, (255, 0, 0), 3)
+            cv2.fillConvexPoly(self.image_mask, convexhull, 255)
+
+            # Delaunay tringulation
+            rect = cv2.boundingRect(convexhull)
+            subdiv = cv2.Subdiv2D(rect)
+            subdiv.insert(landmarks_points)
+            triangels = subdiv.getTriangleList()
+            triangels = np.array(triangels, dtype=np.int32)
+
+            indexes_triangels = []
+
+            for t in triangels:
+                pt1 = (t[0], t[1])
+                pt2 = (t[2], t[3])
+                pt3 = (t[4], t[5])
+
+                index_pt1 = np.where((points == pt1).all(axis=1))
+                index_pt1 = extract_index_nparray(index_pt1)
+                index_pt2 = np.where((points == pt2).all(axis=1))
+                index_pt2 = extract_index_nparray(index_pt2)
+                index_pt3 = np.where((points == pt3).all(axis=1))
+                index_pt3 = extract_index_nparray(index_pt3)
+
+                if index_pt1 is not None and index_pt2 is not None and index_pt3 is not None:
+                    triangle = [index_pt1, index_pt2, index_pt3]
+                    indexes_triangels.append(triangle)
+            return indexes_triangels
+
+
+a = FaceSwap('TestPhotos/Korwin.png')
 
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor('models/shape_predictor_68_face_landmarks.dat')
 
-indexes_triangles = []
-
-# Face 1
-faces = detector(face_1.image_gray)
-for face in faces:
-    landmarks = predictor(face_1.image_gray, face)
-    landmarks_points = []
-    for n in range(0, 68):
-        x = landmarks.part(n).x
-        y = landmarks.part(n).y
-        landmarks_points.append((x, y))
-
-    points = np.array(landmarks_points, np.int32)
-    convexhull = cv2.convexHull(points)
-    # cv2.polylines(img, [convexhull], True, (255, 0, 0), 3)
-    cv2.fillConvexPoly(face_1.image_mask, convexhull, 255)
-
-    face_image_1 = cv2.bitwise_and(face_1.image, face_1.image, mask=face_1.image_mask)
-
-    # Delaunay tringulation
-    rect = cv2.boundingRect(convexhull)
-    subdiv = cv2.Subdiv2D(rect)
-    subdiv.insert(landmarks_points)
-    triangels = subdiv.getTriangleList()
-    triangels = np.array(triangels, dtype=np.int32)
-
-    indexes_triangels = []
-
-    for t in triangels:
-        pt1 = (t[0], t[1])
-        pt2 = (t[2], t[3])
-        pt3 = (t[4], t[5])
-
-        index_pt1 = np.where((points == pt1).all(axis=1))
-        index_pt1 = extract_index_nparray(index_pt1)
-        index_pt2 = np.where((points == pt2).all(axis=1))
-        index_pt2 = extract_index_nparray(index_pt2)
-        index_pt3 = np.where((points == pt3).all(axis=1))
-        index_pt3 = extract_index_nparray(index_pt3)
-
-        if index_pt1 is not None and index_pt2 is not None and index_pt3 is not None:
-            triangle = [index_pt1, index_pt2, index_pt3]
-            indexes_triangels.append(triangle)
+indexes_triangels = a.smth_faces()
+landmarks_points = a.landmarks_points()
 
 while True:
     faces2 = detector(face_2.image_gray)
@@ -159,13 +184,13 @@ while True:
         warped_triangle = cv2.bitwise_and(warped_triangle, warped_triangle, mask=cropped_tr2_mask)
 
         # Reconstruct new face without white lines
-        img2_new_face_rect_area = img2_new_face[y: y + h, x: x + w]
+        img2_new_face_rect_area = face_2_copy[y: y + h, x: x + w]
         img2_new_face_rect_area_gray = cv2.cvtColor(img2_new_face_rect_area, cv2.COLOR_BGR2GRAY)
         _, mask_triangles_designed = cv2.threshold(img2_new_face_rect_area_gray, 1, 255, cv2.THRESH_BINARY_INV)
         warped_triangle = cv2.bitwise_and(warped_triangle, warped_triangle, mask=mask_triangles_designed)
 
         img2_new_face_rect_area = cv2.add(img2_new_face_rect_area, warped_triangle)
-        img2_new_face[y: y + h, x: x + w] = img2_new_face_rect_area
+        face_2_copy[y: y + h, x: x + w] = img2_new_face_rect_area
 
     # Put on new face
     img2_face_mask = np.zeros_like(face_2.image_gray)
@@ -173,7 +198,7 @@ while True:
     img2_face_mask = cv2.bitwise_not(img2_head_mask)
 
     img2_head_noface = cv2.bitwise_and(face_2.image, face_2.image, mask=img2_face_mask)
-    result = cv2.add(img2_head_noface, img2_new_face)
+    result = cv2.add(img2_head_noface, face_2_copy)
 
     # Smoothing transition of images
     (x, y, w, h) = cv2.boundingRect(convexhull2)
